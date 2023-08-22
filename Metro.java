0f701be590e347e3ed3db65430ae9b40ab302233
G Ramya <ramyagajili@gmail.com>
2023-08-22 11:18:20 +0530

    Simple examples for variables and Data types

---
+++ Metro.java
@@ -0,0 +1,16 @@
+class Metro{
+public static void main(String bus[]){
+String cityName = "Navarang";
+int noOfCompartsment = 65;
+String source = "navarang";
+String destination = "JayaNagar";
+int noOfPassengers = 120;
+String ticket = "Rs.25";
+System.out.println("city name is "+ cityName);
+System.out.println("noOfCompartsment in metro "+ noOfCompartsment);
+System.out.println("Source is "+ source);
+System.out.println("Destination is "+ destination);
+System.out.println("noOfPassengerst in metro "+ noOfPassengers);
+System.out.println("Ticket price is  "+ ticket);
+}
+}
