abc6e65fd6088c0bd20ae7c3435dfcd9b7f56bbf
G Ramya <ramyagajili@gmail.com>
2023-08-29 11:54:25 +0530

    commiting classes and Object Related Examples

---
+++ Apartment.java
@@ -0,0 +1,13 @@
+class Apartment{
+
+     int apartmentId ;
+	 String apartmentName ;
+	 String apartmentLocation ;
+	 int noOfFloors ;
+	 int noOfTwoBhkFlats;
+	 int noOfThreeBhkFlats;
+ public static void provideAccomodation(){
+	 System.out.println("Providing Large Space");
+ }
+
+}
---
+++ ApartmentExecutor.java
@@ -0,0 +1,99 @@
+class ApartmentExecutor{
+	
+public static void main (String a[]){
+    //className referenceVariable = new className
+	Apartment apartment1 = new Apartment();
+	apartment1.apartmentId = 234;
+	apartment1.apartmentName = "Prestage";
+	apartment1.apartmentLocation = "WhiteField";
+	apartment1.noOfFloors = 45;
+	apartment1.noOfTwoBhkFlats = 180;
+	apartment1.noOfThreeBhkFlats = 90;
+	apartment1.provideAccomodation();
+	System.out.println("ApartmentId is "+apartment1.apartmentId);
+	System.out.println("Apartment Name is "+apartment1.apartmentName);
+	System.out.println("Apartment Location is "+apartment1.apartmentLocation);
+	System.out.println("No of Floors in Apartment is "+apartment1.noOfFloors);
+	System.out.println("No of Two Bhk Flats in Apartment is "+apartment1.noOfTwoBhkFlats);
+	System.out.println("No of Three Bhk Flats in Apartment is "+apartment1.noOfThreeBhkFlats);
+	System.out.println(
+	
+	"-----------------------------------------------------------------------------------------------------------");
+	
+	
+	Apartment apartment2 = new Apartment();
+	apartment2.apartmentId = 714;
+	apartment2.apartmentName = "Aarvi palace";
+	apartment2.apartmentLocation = "Hassan";
+	apartment2.noOfFloors = 6;
+	apartment2.noOfTwoBhkFlats = 20;
+	apartment2.noOfThreeBhkFlats = 10;
+	apartment2.provideAccomodation();
+	System.out.println("ApartmentId is "+apartment2.apartmentId);
+	System.out.println("Apartment Name is "+apartment2.apartmentName);
+	System.out.println("Apartment Location is "+apartment2.apartmentLocation);
+	System.out.println("No of Floors in Apartment is "+apartment2.noOfFloors);
+	System.out.println("No of Two Bhk Flats in Apartment is "+apartment2.noOfTwoBhkFlats);
+	System.out.println("No of Three Bhk Flats in Apartment is "+apartment2.noOfThreeBhkFlats);
+	
+	System.out.println(
+	
+	"-----------------------------------------------------------------------------------------------------------");
+	
+	
+	Apartment apartment3 = new Apartment();
+	apartment3.apartmentId = 467;
+	apartment3.apartmentName = "Brigade";
+	apartment3.apartmentLocation = "KR Puram";
+	apartment3.noOfFloors = 35;
+	apartment3.noOfTwoBhkFlats = 2000;
+	apartment3.noOfThreeBhkFlats = 120;
+	apartment3.provideAccomodation();
+	System.out.println("ApartmentId is "+apartment3.apartmentId);
+	System.out.println("Apartment Name is "+apartment3.apartmentName);
+	System.out.println("Apartment Location is "+apartment3.apartmentLocation);
+	System.out.println("No of Floors in Apartment is "+apartment3.noOfFloors);
+	System.out.println("No of Two Bhk Flats in Apartment is "+apartment3.noOfTwoBhkFlats);
+	System.out.println("No of Three Bhk Flats in Apartment is "+apartment3.noOfThreeBhkFlats);
+	
+	System.out.println(
+	
+	"-----------------------------------------------------------------------------------------------------------");
+	
+	
+	Apartment apartment4 = new Apartment();
+	apartment4.apartmentId = 802;
+	apartment4.apartmentName = "Shoba";
+	apartment4.apartmentLocation = "Malleshwaram";
+	apartment4.noOfFloors = 39;
+	apartment4.noOfTwoBhkFlats = 160;
+	apartment4.noOfThreeBhkFlats = 40;
+	apartment4.provideAccomodation();
+	System.out.println("ApartmentId is "+apartment4.apartmentId);
+	System.out.println("Apartment Name is "+apartment4.apartmentName);
+	System.out.println("Apartment Location is "+apartment4.apartmentLocation);
+	System.out.println("No of Floors in Apartment is "+apartment4.noOfFloors);
+	System.out.println("No of Two Bhk Flats in Apartment is "+apartment4.noOfTwoBhkFlats);
+	System.out.println("No of Three Bhk Flats in Apartment is "+apartment4.noOfThreeBhkFlats);
+	
+	System.out.println(
+	
+	"-----------------------------------------------------------------------------------------------------------");
+	
+	
+	Apartment apartment5 = new Apartment();
+	apartment5.apartmentId = 79324;
+	apartment5.apartmentName = "United";
+	apartment5.apartmentLocation = "Electromic City";
+	apartment5.noOfFloors = 70;
+	apartment5.noOfTwoBhkFlats = 280;
+	apartment5.noOfThreeBhkFlats = 190;
+	apartment5.provideAccomodation();
+	System.out.println("ApartmentId is "+apartment5.apartmentId);
+	System.out.println("Apartment Name is "+apartment5.apartmentName);
+	System.out.println("Apartment Location is "+apartment5.apartmentLocation);
+	System.out.println("No of Floors in Apartment is "+apartment5.noOfFloors);
+	System.out.println("No of Two Bhk Flats in Apartment is "+apartment5.noOfTwoBhkFlats);
+	System.out.println("No of Three Bhk Flats in Apartment is "+apartment5.noOfThreeBhkFlats);
+}
+}
