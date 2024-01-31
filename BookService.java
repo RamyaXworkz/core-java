2c16e43f948b8cb3554d4ffbde352f2a5c1cf57b
G Ramya <ramyagajili@gmail.com>
2024-01-31 18:04:51 +0530

    commiting crud on jdbc

--- BookService.java
+++ BookService.java
@@ -2,6 +2,14 @@ package com.xworkz.book.service;
 
 import com.xworkz.book.dto.BookDto;
 
+import java.sql.SQLException;
+
 public interface BookService {
     public boolean validateAndSave(BookDto dto);
+    public BookDto findById(int id);
+
+    public  BookDto findByName(String bookName) throws SQLException;
+
+    public  boolean updateVersionByBookName( String bookName ,String version) ;
+    public boolean deleteBookNameById(int id );
 }
